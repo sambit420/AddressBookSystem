2145e31f588c3c4afa3a5a44d726af69d3979cbd
sambit420 <sambit2024@gmail.com>
2022-10-24 02:06:45 +0530

    Added : Use Case 1 - Create a contact in Address Book

--- src/com/address/book/AddressBookSystemMain.java
+++ src/com/address/book/AddressBookSystemMain.java
@@ -10,5 +10,11 @@ public class AddressBookSystemMain {
 
         //Initialize Object
         AddressBookSystemMain obj = new AddressBookSystemMain();
+        //Calling Encapsulated Class object
+        AddressBook addressBook = new AddressBook("Sambit","kumar","choudwar","Cuttack",
+                "Odisha","754025","7008642521","sambit2024@gmail.com");
+
+        System.out.print("\n"+addressBook.toString());
+
     }
 }
