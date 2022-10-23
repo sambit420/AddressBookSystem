3fa59ca70b391b446eb42ecc28e9fbce3264e45d
sambit420 <sambit2024@gmail.com>
2022-10-24 01:49:40 +0530

    Added : Display Welcome to Address Book Program

---
+++ src/com/address/book/AddressBookSystemMain.java
@@ -0,0 +1,14 @@
+package com.address.book;
+
+public class AddressBookSystemMain {
+    //Default Constructor
+    public AddressBookSystemMain() {
+        System.out.println("Welcome to Address Book Program !!!");
+    }
+
+    public static void main(String[] args) {
+
+        //Initialize Object
+        AddressBookSystemMain obj = new AddressBookSystemMain();
+    }
+}
